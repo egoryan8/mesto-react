6c123a2caf8b456e7d3cc275b1164e6b6994e121
egoryan8 <fogel_92@bk.ru>
2022-09-10 22:03:22 +0700

    feat: add CurrentUserContext in Card, make like, delete btns

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import CurrentUserContext from '../contexts/CurrentUserContext';
 
 const Card = ({ link, name, likesCount, onCardClick, card }) => {
+  const { _id } = React.useContext(CurrentUserContext);
+  const isOwn = card.owner._id === _id;
+  const isLiked = card.likes.some((i) => i._id === _id);
+  const cardLikeButtonClassName = `card__like-btn ${isLiked ? 'card__like-btn_active' : ''}`;
+
   return (
     <li className="card">
       <img src={link} alt={name} className="card__image" onClick={() => onCardClick(card)} />
-      <button type="button" className="card__delete-btn"></button>
+      {isOwn && <button type="button" className="card__delete-btn"></button>}
       <div className="card__title-wrapper">
         <h2 className="card__title">{name}</h2>
         <div className="card__like-wrapper">
-          <button className="card__like-btn" type="button" aria-label="Поставить лайк"></button>
+          <button
+            className={cardLikeButtonClassName}
+            type="button"
+            aria-label="Поставить лайк"></button>
           <p className="card__likes-count">{likesCount}</p>
         </div>
       </div>
