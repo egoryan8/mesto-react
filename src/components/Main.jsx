afd24db0738b12c7c319660e333938916f1e9b35
egoryan8 <fogel_92@bk.ru>
2022-09-10 21:54:33 +0700

    feat: add CurrentUserContext to main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
+import CurrentUserContext from '../contexts/CurrentUserContext';
 import { api } from '../utils/api';
 import Card from './Card';
 
 const Main = ({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) => {
-  const [userName, setUserName] = React.useState('');
-  const [userDescription, setUserDescription] = React.useState('');
-  const [userAvatar, setUserAvatar] = React.useState('');
+  const { name, about, avatar } = React.useContext(CurrentUserContext);
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
-    api
-      .getProfile()
-      .then((res) => {
-        setUserName(res.name);
-        setUserDescription(res.about);
-        setUserAvatar(res.avatar);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
     api
       .getCards()
       .then((res) => setCards(...cards, res))
@@ -30,14 +18,14 @@ const Main = ({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) => {
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-wrapper">
-          <img src={userAvatar} alt="Аватар профиля" className="profile__avatar" />
+          <img src={avatar} alt="Аватар профиля" className="profile__avatar" />
           <div className="profile__avatar-edit" onClick={onEditAvatar}></div>
         </div>
 
         <div className="profile__info">
-          <h1 className="profile__name">{userName}</h1>
+          <h1 className="profile__name">{name}</h1>
           <button type="button" className="profile__edit-btn" onClick={onEditProfile}></button>
-          <p className="profile__status">{userDescription}</p>
+          <p className="profile__status">{about}</p>
         </div>
         <button
           onClick={onAddPlace}
