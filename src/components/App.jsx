41d7419128cf9f971ba23a64b2fc723d6a286891
egoryan8 <fogel_92@bk.ru>
2022-09-11 16:56:49 +0700

    refactor: remove unused imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 import { api } from '../utils/api';
 import CurrentUserContext from '../contexts/CurrentUserContext';
